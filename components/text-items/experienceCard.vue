1a717fa4989caa65d8e564879f74aa0b2b131b70
bigstth <sitthidsn@gmail.com>
2021-01-01 02:14:58 +0700

    Experiences Page

---
+++ components/text-items/experienceCard.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <Card :bordered="false" shadow :style="{ margin: '4px 4px 8px 4px' }">
+    <h2
+      slot="title"
+      style="text-align: center; font-size: 16px; line-height: 24px"
+    >
+      {{ expCardInfo.title }}
+    </h2>
+    <div style="display: flex; justify-content: center">
+      <img
+        :src="expCardInfo.imgUrl"
+        :alt="expCardInfo.title"
+        class="expCardImg"
+      />
+    </div>
+  </Card>
+</template>
+
+<script>
+export default {
+  props: {
+    expCardInfo: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.expCardImg {
+  max-width: 460px;
+  width: 100%;
+  max-height: 350px;
+  object-fit: cover;
+  border-radius: 5px;
+}
+</style>
--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,23 +1,98 @@
 <template lang="html">
-  <div class="container">
+  <div class="exp-container">
     <subpageHeader class="subpageHeader">
       <template slot="headerTitle">Experiences</template>
       <template slot="navigationName">Experiences</template>
     </subpageHeader>
+    <Row class="expCardSection">
+      <div v-for="expCardInfo in expCardInfos" :key="expCardInfo.id" class="">
+        <experienceCard
+          :expCardInfo="expCardInfo"
+          class="expCard"
+        ></experienceCard>
+      </div>
+    </Row>
   </div>
 </template>
 
 <script>
 import subpageHeader from '@/components/subpageHeader'
+import experienceCard from '@/components/text-items/experienceCard'
 export default {
   components: {
     subpageHeader,
+    experienceCard,
+  },
+  data() {
+    return {
+      expCardInfos: [
+        {
+          id: 1,
+          title: 'Has been staff of "BU OPEN HOUSE" ',
+          imgUrl: require('@/assets/images/contents/openHouse.jpg'),
+        },
+        {
+          id: 2,
+          title: 'Has Been a Teacher Assistant in "GENERAL EDUCATION" subject',
+          imgUrl: require('@/assets/images/contents/GEclass.jpg'),
+        },
+        {
+          id: 3,
+          title: 'Has Been a Staff of Team Organizing "BU E-SPORTS SEMINAR"',
+          imgUrl: require('@/assets/images/contents/IMG_2821-2048x1536.jpg'),
+        },
+        {
+          id: 4,
+          title:
+            'Worked Part-Time job at "THE PIZZA COMPANY" as Customer Service',
+          imgUrl: require('@/assets/images/contents/pizza-379x485.png'),
+        },
+        {
+          id: 5,
+          title:
+            'Has Been a Staff of Organizing Team "BU E-SPORTS COMPETITIONS"',
+          imgUrl: require('@/assets/images/contents/IMG_3146-scaled-379x485.jpg'),
+        },
+        {
+          id: 6,
+          title: 'Good Study Award 2019',
+          imgUrl: require('@/assets/images/contents/1.png'),
+        },
+        {
+          id: 7,
+          title: 'Good Study Award 2020',
+          imgUrl: require('@/assets/images/contents/2.png'),
+        },
+      ],
+    }
   },
 }
 </script>
 
 <style lang="css" scoped>
-.container {
-  height: 100vh;
+.exp-container {
+  height: 100%;
+}
+.expCardSection {
+  background: #f7f7f7;
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.expCardLists {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.expCard {
+  width: 450px;
+}
+@media screen and (max-width: 500px) {
+  .expCard {
+    width: 350px;
+  }
 }
 </style>
